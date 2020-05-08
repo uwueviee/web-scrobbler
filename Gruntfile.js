e5307de982adf9da7d07bad0663f1b4176ad6175
alexesprit <alex.esprit@gmail.com>
2020-05-08 17:14:19 +0300

    Add Grunt task to validate HTML files

--- Gruntfile.js
+++ Gruntfile.js
@@ -177,6 +177,9 @@ module.exports = (grunt) => {
 				fix: !isCi,
 			},
 		},
+		htmlvalidate: {
+			src: HTML_FILES,
+		},
 		jsonlint: {
 			src: JSON_FILES,
 		},
@@ -219,6 +222,7 @@ module.exports = (grunt) => {
 
 	require('jit-grunt')(grunt, {
 		mochacli: 'grunt-mocha-cli',
+		htmlvalidate: 'grunt-html-validate',
 	});
 	grunt.loadTasks('.grunt');
 
@@ -306,7 +310,7 @@ module.exports = (grunt) => {
 	 * Lint source code using linters specified below.
 	 */
 	grunt.registerTask('lint', [
-		'eslint', 'jsonlint', 'lintspaces',
+		'eslint', 'jsonlint', 'lintspaces', 'htmlvalidate',
 		'stylelint', 'remark', 'unused_files',
 	]);
 
