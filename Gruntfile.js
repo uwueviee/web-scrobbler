7081abc4999bf2043556aae4e4b63c6bfb1fc7ef
alexesprit <alex.esprit@gmail.com>
2020-06-24 20:47:54 +0300

    Lint Vue components

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,10 @@ const JS_FILES = [
 const JSON_FILES = [`${srcDir}/**/*.json`, '*.json'];
 const HTML_FILES = [`${srcDir}/ui/**/*.html`];
 const CSS_FILES = [`${srcDir}/ui/**/*.css`];
-const DOC_FILES = ['*.md', '.github/**/*.md'];
+const VUE_FILES = [`${srcDir}/ui/**/*.vue`];
+const DOC_FILES = [
+	'*.md', '.github/**/*.md',
+];
 
 const isCi = process.env.CI === 'true';
 
@@ -130,19 +133,19 @@ module.exports = (grunt) => {
 		 */
 
 		eslint: {
-			target: JS_FILES,
+			target: [JS_FILES, VUE_FILES],
 			options: {
 				fix: !isCi,
 			},
 		},
 		htmlvalidate: {
-			src: HTML_FILES,
+			src: [HTML_FILES, VUE_FILES],
 		},
 		jsonlint: {
 			src: JSON_FILES,
 		},
 		lintspaces: {
-			src: [JS_FILES, JSON_FILES, CSS_FILES, HTML_FILES],
+			src: [JS_FILES, JSON_FILES, CSS_FILES, HTML_FILES, VUE_FILES],
 			options: {
 				editorconfig: '.editorconfig',
 				ignores: ['js-comments'],
@@ -156,7 +159,7 @@ module.exports = (grunt) => {
 			src: DOC_FILES,
 		},
 		stylelint: {
-			all: CSS_FILES,
+			all: [CSS_FILES, VUE_FILES],
 		},
 
 		/**
