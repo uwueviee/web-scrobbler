62f5b41c89d0d96409cf7bd54cba7abbc4cdbbed
alexesprit <alex.esprit@gmail.com>
2020-07-25 19:09:09 +0300

    Use Node.js style for preprocess directives
    Replace DEBUG and RELEASE with DEVELOPMENT and PRODUCTION respectively.

--- src/content/connector.js
+++ src/content/connector.js
@@ -634,7 +634,7 @@ function BaseConnector() {
 		originUrl: null,
 	};
 
-	/* @ifdef DEBUG */
+	/* @ifdef DEVELOPMENT */
 	/**
 	 * List of song fields used to check if song is changed. If any of
 	 * these fields are changed, the new song is playing.
@@ -712,7 +712,7 @@ function BaseConnector() {
 				this.reactorCallback(filteredState, changedFields);
 			}
 
-			// @ifdef DEBUG
+			// @ifdef DEVELOPMENT
 			if (changedFields.includes('isPlaying')) {
 				Util.debugLog(`isPlaying state changed to ${newState.isPlaying}`);
 			}
